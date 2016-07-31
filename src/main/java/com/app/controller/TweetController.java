5c60683936e97a68ef05713f5007be9764fe87e8
= <giecmarcin@gmail.com>
2016-07-31 19:54:40 +0200

    init commit

---
+++ src/main/java/com/app/controller/TweetController.java
@@ -0,0 +1,33 @@
+package com.app.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * Created by Marcin on 31.07.2016.
+ */
+
+@Controller
+public class TweetController {
+
+    @Autowired
+    private Twitter twitter;
+
+    @RequestMapping("/")
+    public String hello(@RequestParam(defaultValue = "Spring MVC4") String search, Model model){
+        SearchResults searchResults = twitter.searchOperations().search(search);
+        List<Tweet> tweets = searchResults.getTweets();
+        model.addAttribute("tweets", tweets);
+        model.addAttribute("search", search);
+        return "resultPage";
+    }
+}
