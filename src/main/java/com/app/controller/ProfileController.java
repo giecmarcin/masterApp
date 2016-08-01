1161d4e68de05e079d3e73bea9b38241768707c6
Marcin Giec <giecmarcin@gmail.com>
2016-08-01 22:07:56 +0200

    //

--- src/main/java/com/app/controller/ProfileController.java
+++ src/main/java/com/app/controller/ProfileController.java
@@ -1,7 +1,15 @@
 package com.app.controller;
 
+import com.app.dto.ProfileForm;
+import com.app.services.UsLocalDateFormatter;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+import java.util.Locale;
 
 /**
  * Created by Marcin on 31.07.2016.
@@ -10,7 +18,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ProfileController {
     @RequestMapping("/profile")
-    public String displayProfile() {
+    public String displayProfile(ProfileForm profileForm) {
         return "profile/profilePage";
+    }
+
+    @RequestMapping(value = "/profile", method = RequestMethod.POST)
+    public String saveProfile(@Valid ProfileForm  profileForm, BindingResult result){
+        if(result.hasErrors()){
+            return "/profile/profilePage";
+        }
+        System.out.println("Pomyślnie zapisano");
+        return  "redirect:/profile";
+    }
+
+    @ModelAttribute("dateFormat")
+    public String localeFormat(Locale locale) {
+        return UsLocalDateFormatter.getPattern(locale);
     }
 }
--- src/main/java/com/app/dto/ProfileForm.java
+++ src/main/java/com/app/dto/ProfileForm.java
@@ -3,14 +3,28 @@ package com.app.dto;
 /**
  * Created by Marcin on 31.07.2016.
  */
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProfileForm {
+
+    @Size(min = 2)
     private String twitterHandle;
+
+    @Email
+    @NotEmpty
     private String email;
+
+    @NotNull
     private LocalDate birthDate;
+
+    @NotEmpty
     private List<String> tastes = new ArrayList<>();
 
     public String getTwitterHandle() {
